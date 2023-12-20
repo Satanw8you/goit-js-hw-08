f1cfd0678f95aa982db7703900655aac5d1aed00
Satanw8you <trusnikolaj41@gmail.com>
2023-12-20 13:56:39 +0200

    finish task

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-const allLiEl = document.querySelectorAll('.item');
-console.log(`Numbers of categories: ${allLiEl.length}`);
-
-allLiEl.forEach(category => {
-  const categoryName = category.querySelector('h2').textContent;
-  const itemsCount = category.querySelectorAll('ul li').length;
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${itemsCount}`);
-});
